97304c84417467befcca302b15bf74d8877f83d9
francis jay <francis.bedes@novare.com.hk>
2024-03-20 16:39:49 +0800

    about

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/pages/Home'
+import About from './components/pages/About'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -20,14 +21,15 @@ function App() {
           </div>
           <div className="content">
             <Routes>
-            <Route path="/" element={<Navigate to="fr-ncisj-y.github.io/jay-plusplus/" replace={true} />} />
+              <Route path="/" element={<Navigate to="fr-ncisj-y.github.io/jay-plusplus/" replace={true} />} />
               <Route path="jay-plusplus/" element={<Home />} />
+              <Route path="jay-plusplus/about" element={<About />} />
             </Routes>
           </div>
           <div className="socials">
-            <a href='' className='messenger'><FontAwesomeIcon icon={faFacebookMessenger} /></a>
-            <a href='' className='linkedin'><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href='' className='github'><FontAwesomeIcon icon={faGithub} /></a>
+            <a href='#' className='messenger'><FontAwesomeIcon icon={faFacebookMessenger} /></a>
+            <a href='#' className='linkedin'><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href='#' className='github'><FontAwesomeIcon icon={faGithub} /></a>
           </div>
         </div>
       </div>
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+function About() {
+  return (
+    <div className='content-container'>
+        <div className="container-text">
+        <h1>nice to meet you</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
